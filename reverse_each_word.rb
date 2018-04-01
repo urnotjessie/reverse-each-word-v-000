bb076c1528ec7abe8eb975a3ab6e219bd6c76717
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 04:08:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
     sentence_to_array = sentence.split(" ")
     sentence_to_array.collect do |word|
-        result << word.reverse
+        word.reverse
     end
-    return result.join(" ")
 end
